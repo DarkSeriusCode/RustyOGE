15795637c237a52c9b00c0c6a00432fc1684722d
DarkSeriusCode <darkseriusw@gmail.com>
2022-12-05 22:37:46 +0300

    module(tasks/task2): Added solution for 2nd task

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+
+/// Helps you to solve OGE tasks
+#[derive(Parser)]
+struct CLI {
+    /// The number of the task
+    number: usize
+}
+
+pub mod tasks;
+pub use tasks::task2;
+
 fn main() {
-    println!("Hello, world!");
+    let args = CLI::parse();
+
+    solve_by_num(args.number).unwrap_or_else(|err| eprintln!("{err}"));
+}
+
+fn solve_by_num(number: usize) -> Result<(), &'static str> {
+    match number {
+        2 => Ok(task2::solve()?),
+        _ => Err("Cannot find the task"),
+    }
 }
