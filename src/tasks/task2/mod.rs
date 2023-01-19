713b3817eca447570f13c146ffd8fd6cc9d311a7
DarkSeriusCode <stepanharkov19@gmail.com>
2023-01-19 20:13:25 +0300

    style(task2/mod): Small code improvement

--- src/tasks/task2/mod.rs
+++ src/tasks/task2/mod.rs
@@ -12,15 +12,14 @@ where
     if type_num > solvers::SOLVERS_COUNT {
         return Err("Задание 2: Неизвестный тип задачи! (см README.md)")
     }
-    let input_data = input()?;
-    let (codes, strings) = (input_data.codes, input_data.encoded_strings);
+    let types::InputData{ codes, encoded_strings } = input()?;
     // Решаем задачу, если ошибка, то возвращаем её
     let answer = match type_num {
-        1 => solvers::solve_type1(codes, strings),
-        2 => solvers::solve_type2(codes, strings),
-        3 => solvers::solve_type3(codes, strings),
-        4 => solvers::solve_type4(codes, strings),
-        5 => solvers::solve_type5(codes, strings),
+        1 => solvers::solve_type1(codes, encoded_strings),
+        2 => solvers::solve_type2(codes, encoded_strings),
+        3 => solvers::solve_type3(codes, encoded_strings),
+        4 => solvers::solve_type4(codes, encoded_strings),
+        5 => solvers::solve_type5(codes, encoded_strings),
         _ => todo!(),
     }?;
     Ok(answer)
