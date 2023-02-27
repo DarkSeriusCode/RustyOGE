dac465d000d73db1c095c79e5b030b1a91d6ee3d
DarkSeriusCode <stepanharkov19@gmail.com>
2023-02-27 19:49:05 +0300

    cli(input): Add input() function for a single user input

--- src/input.rs
+++ src/input.rs
@@ -1,7 +1,18 @@
-use std::io;
+use std::io::{self, Write};
 use std::collections::HashMap;
 use rusty_oge::task2;
 use crate::errors::CLIError;
+
+// Выводит `prompt` и ждёт ввод пользователя
+fn input(prompt: &str) -> Result<String, CLIError> {
+    print!("{prompt}");
+    io::stdout().flush().unwrap();
+    let mut buff = String::new();
+    if let Err(_) = io::stdin().read_line(&mut buff) {
+        return Err(CLIError::ReadingError);
+    }
+    Ok(buff.trim().to_string())
+}
 
 /// Выводит `prompt` и читает строки из `stdin` пока не будет введено end
 fn input_until_end(prompt: &str) -> Result<Vec<String>, CLIError> {
@@ -9,22 +20,18 @@ fn input_until_end(prompt: &str) -> Result<Vec<String>, CLIError> {
     let mut strings = Vec::new();
 
     loop {
-        let mut buff = String::new();
-        if let Err(_) = io::stdin().read_line(&mut buff) {
-            return Err(CLIError::ReadingError);
-        }
-        buff = buff.trim().to_string();
+        let buff = input("")?;
         if buff == "end" { break; }
         strings.push(buff);
     }
     Ok(strings)
 }
 
-// ---------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------
 
 pub fn read_task2_input() -> Result<task2::InputData, CLIError> {
     let mut codes: HashMap<String, String> = HashMap::new();
-    // Getting codes
+
     for s in input_until_end("Введите букву и код через пробел")? {
         let pair = Vec::from_iter(s.split_whitespace());
         if pair.len() != 2 {
@@ -37,4 +44,7 @@ pub fn read_task2_input() -> Result<task2::InputData, CLIError> {
 
     Ok(task2::InputData::new(codes, encoded_strings))
 }
+
+// --------------------------------------------------------------------------------------
+
 
